5ee6556230c40f045038bb7b98984ec5f12f8a81
violet <126717281+P-skylos@users.noreply.github.com>
2024-08-23 18:30:46 -0700

    basic compiler done
    takes basic go / spin commands and translates them to EDPY commands.

--- mincontrol_transformer.js
+++ mincontrol_transformer.js
@@ -4,30 +4,59 @@ DIR_ANGLE.set("backward", 180)
 DIR_ANGLE.set("right", 90)
 DIR_ANGLE.set("left", -90)
 
+const DIR_ED =new Map([])
+
 function compile(ast){
     switch(ast.type){
     case("sequence"):
-        s1 = compile(ast.left)
-        s2 = compile(ast.right)
+        let s1 = compile(ast.left)
+        let s2 = compile(ast.right)
         return `${s1}\n${s2}`
         break
     case("go"):
-        drive = `Ed.Drive()`
+        return compile_go(ast)
+        break
     case("spin"):
         return compile_spin(ast)
+        break
     }
 }
 
-function compile_spin(ast){
-    angle = ast.duration
+function compile_go(ast){
     switch(ast.direction){
+        case("forward"):
+            return `Ed.DRIVE(Ed.FORWARD, Ed.SPEED_5, ${ast.duration})`
+            break
+        case("backward"):
+            return `Ed.DRIVE(Ed.BACKWARD, Ed.SPEED_5, ${ast.duration})`
+            break
         case("left"):
+            spin = compile_spin({type:"spin", direction:"left", duration:90})
+            forward = `Ed.DRIVE(Ed.FORWARD, Ed.SPEED_5, ${ast.duration})`
+            return `${spin}\n${forward}`
             break
         case("right"):
-            break
-        default:
+            spin = compile_spin({type:"spin", direction:"right", duration:90})
+            forward = `Ed.DRIVE(Ed.FORWARD, Ed.SPEED_5, ${ast.duration})`
+            return `${spin}\n${forward}`
             break
     }
+    throw Error("unexpected input in compile_go")
+}
+
+function compile_spin(ast){
+    direction = '';
+    switch(ast.direction){
+        case("left"):
+            direction = "Ed.SPIN_LEFT"
+            break
+        case("right"):
+            direction = "Ed.SPIN_RIGHT"
+            break
+        default:
+            throw new Error(`spin direction "${ast.direction}" is undefined`)
+    }
+    return `Ed.Drive(${direction}, Ed.SPEED_5, ${ast.duration})`
 }
 
 
@@ -36,4 +65,4 @@ function transform(ast){
 }
 exports.transform = transform
 
-export.compile = compile
+exports.compile = compile
